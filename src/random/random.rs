618c6ea041b024ea1ee50f45ca35a075313a0677
Jonas Carpay <jonascarpay@gmail.com>
2026-05-15 23:20:30 +0900

    Rng trait

--- src/random/random.rs
+++ src/random/random.rs
@@ -1,7 +1,28 @@
-use std::ops::RangeBounds;
+use std::ops::{RangeBounds, RangeTo};
 
 use crate::random::rng::Rng;
 
-pub trait Uniform<T>: RangeBounds<T> {
-    fn draw<R: Rng>(&mut self) -> T;
+pub trait Uniform<T> {
+    fn draw<R: Rng>(self, rng: &mut R) -> T;
+}
+
+pub trait UniformExt<T, UR, UT> {
+    fn uniform(&mut self, range: UR) -> UT;
+}
+
+impl<T: Rng, UR: Uniform<UT>, UT> UniformExt<T, UR, UT> for T {
+    fn uniform(&mut self, range: UR) -> UT {
+        range.draw(self)
+    }
+}
+
+impl Uniform<usize> for RangeTo<usize> {
+    fn draw<R: Rng>(self, rng: &mut R) -> usize {
+        loop {
+            let r = rng.next_u64() as usize;
+            if r < self.end {
+                return r;
+            }
+        }
+    }
 }
