4144a6b5855e04dd621342857ad1f4d6ccd308df
Jonas Carpay <jonascarpay@gmail.com>
2026-05-30 13:32:07 +0900

    unorm tests

--- graphics/src/color/unorm8.rs
+++ graphics/src/color/unorm8.rs
@@ -1,19 +1,49 @@
-#[derive(Debug, Clone, Copy)]
-pub struct Unorm8 {
-    unscaled: u8,
-}
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Unorm8(pub u8);
 
 impl Unorm8 {
+    pub const ZERO: Unorm8 = Unorm8(0u8);
+    pub const ONE: Unorm8 = Unorm8(u8::MAX);
+    pub const EPSILON: Unorm8 = Unorm8(1u8);
     pub fn mult_blinn(self, rhs: Self) -> Self {
-        let xy = self.unscaled as u16 * rhs.unscaled as u16 + 128;
-        Unorm8 {
-            unscaled: ((xy + (xy >> 8)) >> 8) as u8,
-        }
+        let xy = self.0 as u16 * rhs.0 as u16 + 128;
+        Unorm8(((xy + (xy >> 8)) >> 8) as u8)
     }
     pub fn mult_ref(self, rhs: Self) -> Self {
-        let xy = self.unscaled as u16 * rhs.unscaled as u16;
-        Unorm8 {
-            unscaled: ((xy + 127) / 255) as u8,
+        let xy = self.0 as u16 * rhs.0 as u16;
+        Unorm8(((xy + 127) / 255) as u8)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use proptest::prelude::*;
+
+    impl Arbitrary for Unorm8 {
+        type Parameters = ();
+        type Strategy = BoxedStrategy<Self>;
+
+        fn arbitrary_with(_: ()) -> Self::Strategy {
+            any::<u8>().prop_map(Unorm8).boxed()
         }
+    }
+
+    proptest! {
+        #[test]
+        fn mult_blinn_matches_ref(a: Unorm8, b: Unorm8) {
+            prop_assert_eq!(a.mult_blinn(b), a.mult_ref(b));
+        }
+
+        #[test]
+        fn mult_identity(a: Unorm8) {
+            prop_assert_eq!(a.mult_blinn(Unorm8::ONE), a);
+        }
+
+        #[test]
+        fn mult_zero(a: Unorm8) {
+            prop_assert_eq!(a.mult_blinn(Unorm8::ZERO), Unorm8::ZERO);
+        }
+
     }
 }
